e6bd65ea9e968bf1b7038fbbccba6f20a5163129
JowitaKozlak <62951367+JowitaKozlak@users.noreply.github.com>
2020-06-21 15:37:46 +0200

    Unnecessary lines removed

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#from Event import Event
 from random import seed
 from random import randint
 seed(1)
@@ -6,7 +5,7 @@ seed(1)
 # time in s; speed in bps; size in B
 bucket_size = 600
 bucket = bucket_size
-p_size = 0
+p_size = 100 #value zero indicates random packets size
 rate = 24000
 exceed = 0
 conform = 0
@@ -19,10 +18,8 @@ p_num = 22
 packets = []
 i = 0
 pr = 48000
-t_on = 0.2
-t_off = 0.2
-#t_on = bucket_size*8/(pr-rate)
-#t_off = bucket_size*8/rate
+t_on = bucket_size*8/(pr-rate)
+t_off = bucket_size*8/rate
 counter = 0
 
 if p_size != 0:
@@ -34,15 +31,14 @@ else:
         i += 1
     print(packets)
 
-#'%.2f'%
 j = 0
-step = 0.02
+step = round(p_size*8/pr,2) #step for constant packets size
 while currentTime <= endTime:
     if counter == t_on + 0.02:
         currentTime = round(currentTime + t_off - 0.02,2)
         counter = 0
     else:
-        #step = round(packets[j]*8/pr,2)
+        #step = round(packets[j]*8/pr,2) #step for random packets size
         print ("ct = ", currentTime)
         time_interval = round(currentTime - oldTime,2)
         print ("ti = ", time_interval)
@@ -59,7 +55,7 @@ while currentTime <= endTime:
         j += 1
         oldTime = currentTime
         counter = round(counter + step,2)
-        currentTime = round(currentTime + step,2) #krok jaki sobie wybierzemy
+        currentTime = round(currentTime + step,2)
 	
 
 if __name__ == '__main__':
